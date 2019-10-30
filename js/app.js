33a3cfb986463b44dbf0beaf72c844c1a95a65a2
LindsayPeltier <lindsaypeltier@gmail.com>
2019-10-30 10:43:23 -0700

    photos render correctly

--- js/app.js
+++ js/app.js
@@ -8,20 +8,24 @@ function Photo(photo) {
   this.keyword = photo.keyword;
   this.horns = photo.horns;
 }
-photo.allphotos = [];
+Photo.allPhotos = [];
 
 Photo.prototype.render = function() {
 
   //1. Create Element
   let photoClone = $('#photo-template').clone();
   console.log(photoClone);
-  let $photoClone = $(photoClone[0].content);
+  let $photoClone = $(photoClone[0]);
   console.log($photoClone);
 
   //2. Give it Content
   $photoClone.find('h2').text(this.title);
   $photoClone.find('img').attr('src', this.image_url);
   $photoClone.find('p').text(this.description);
+  $photoClone.find('p').text(this.keyword);
+  $photoClone.find('p').text(this.horns);
+  $photoClone.removeClass('clone');
+  $photoClone.attr('class', this.title);
 
   //3. Append to the DOM
   $photoClone.appendTo('main');
@@ -32,8 +36,8 @@ Photo.readJson = () => {
   $.get('./data/page-1.json')
     .then(data => {
       data.forEach(item=> {
-        Photo.allphotos.push(new Photo(item));
-        console.log(item);
+        Photo.allPhotos.push(new Photo(item));
+        //console.log(item);
       });
     })
     .then(Photo.loadPhotos);
